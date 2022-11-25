9295a449d7a774c4b4abd5fb7dd8b0c96e384c82
Elvis Perlika <elvis>
2022-11-25 13:56:45 +0100

    Implemented the Controller

--- 82-simple-mvc-io/src/main/java/it/unibo/mvc/Controller.java
+++ 82-simple-mvc-io/src/main/java/it/unibo/mvc/Controller.java
@@ -1,9 +1,41 @@
 package it.unibo.mvc;
 
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
 
 /**
  * Application controller. Performs the I/O.
  */
 public class Controller {
 
+    public static final String SEP = File.separator;
+    public static final String DEFAULT_PATH = 
+		System.getProperty("Users") + SEP + "elvisperlika" + SEP + "Desktop" + SEP + "FileTest.txt";
+
+    FileWriter currentFile;
+    public Controller() {
+        try {
+            this.currentFile = new FileWriter(DEFAULT_PATH);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    final FileWriter getCurrentFile() {
+        return currentFile;
+    }
+    
+    final String getPath() {
+        return DEFAULT_PATH;
+    }
+
+    final void writeOnFile(String inString) {
+        try {
+            currentFile.write(inString);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+    
 }
